ba39480ec0e939cecffbf3d6b040b38410c1bb90
bandeira <tatasha_oliver@hotmail.com>
2025-04-02 01:16:17 -0300

    Creating level one

--- code/Level.py
+++ code/Level.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
 from code.Entity import Entity
+from code.EntityFactory import EntityFactory
 
 
 class Level:
@@ -8,7 +11,15 @@ class Level:
         self.window = window
         self.game_mode = name
         self.game_mode = game_mode
-        self.entity_list = list[Entity] = []
+        self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
-    def run(self, ):
-        pass
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+            pygame.display.flip()
+            pass
+
+
