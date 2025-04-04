61abcf1ce527ceb61d7aa474e0452362ec67746a
bandeira <tatasha_oliver@hotmail.com>
2025-04-03 22:17:10 -0300

    Creating level one

--- code/Level.py
+++ code/Level.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
 import pygame
 
+from pygame import Surface, Rect
+from pygame.font import Font
+from pygame.examples.grid import WINDOW_HEIGHT
+
+from code.Const import COLOR_WHITE, WIN_HEIGHT, COLOR_BLACK
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -9,17 +15,42 @@ from code.EntityFactory import EntityFactory
 class Level:
     def __init__(self, window, name, game_mode):
         self.window = window
-        self.game_mode = name
+        self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000
 
     def run(self):
+        pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(120)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+
+                # printed text
+                self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_BLACK, (10, 5))
+                self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_BLACK, (10, WIN_HEIGHT - 35))
+                self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_BLACK, (10, WINDOW_HEIGHT - 20))
+
             pygame.display.flip()
-            pass
 
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+
+
+                    pygame.display.flip()
+                pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Comic Sans MS", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
