3f8ffc1795d702d5ec898d5ddea966dee663c730
bandeira <tatasha_oliver@hotmail.com>
2025-04-04 00:48:12 -0300

    Creating level one

--- code/Const.py
+++ code/Const.py
@@ -1,3 +1,6 @@
+import pygame
+from pygame import USEREVENT
+
 #C
 COLOR_GREEN = (173,255,47)
 COLOR_YELLOW = (255,255,0)
@@ -7,6 +10,7 @@ COLOR_BLACK = (0,0,0)
 
 
 # E
+EVENT_ENEMY = pygame.USEREVENT +1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -15,6 +19,10 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1' : 2,
+    'Enemy2' : 1,
 }
 #M
 MENU_OPTION = ('NEW GAME 1P',
@@ -23,8 +31,25 @@ MENU_OPTION = ('NEW GAME 1P',
                'SCORE',
                'EXIT')
 
+#P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+
+
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 
 #W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
 
--- code/Level.py
+++ code/Level.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
 import sys
+from random import choice
+
 import pygame
 
 from pygame import Surface, Rect
 from pygame.font import Font
 from pygame.examples.grid import WINDOW_HEIGHT
 
-from code.Const import COLOR_WHITE, WIN_HEIGHT, COLOR_BLACK
+from code.Const import COLOR_WHITE, WIN_HEIGHT, COLOR_BLACK, TIMEOUT_LEVEL, MENU_OPTION, EVENT_ENEMY
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -19,23 +22,27 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-        self.timeout = 20000
-
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        self.timeout = TIMEOUT_LEVEL
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+        pygame.time.set_timer(EVENT_ENEMY,2000)
     def run(self):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
+
         while True:
             clock.tick(120)
+
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
 
                 # printed text
-                self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_BLACK, (10, 5))
-                self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_BLACK, (10, WIN_HEIGHT - 35))
-                self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_BLACK, (10, WINDOW_HEIGHT - 20))
-
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_BLACK, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_BLACK, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_BLACK, (10, WINDOW_HEIGHT - 20))
             pygame.display.flip()
 
 
@@ -43,11 +50,10 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
-
-
-                    pygame.display.flip()
-                pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Comic Sans MS", size=text_size)
